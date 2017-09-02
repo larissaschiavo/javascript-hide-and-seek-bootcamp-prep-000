1633596a779b7063248e87dbf401eb1943a162f4
Larissa S <larissafschiavo@gmail.com>
2017-09-02 00:25:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,31 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const allLists = document.querySelectorAll('.ranked-list')
+
+  for(let i = 0; i < allLists.length; i++) {
+    let listbabies = allLists[i].children
+    for(let j = 0; j < listbabies.length; j++){
+      listbabies[j].innerHTML = parseInt(listbabies[j].innerHTML) + n
+    }
+  }
+}
+
+
+function deepestChild() {
+  let mainNode = document.getElementById('grand-node')
+  let nextNode = mainNode.children[0]
+
+    while(nextNode) {
+      mainNode = nextNode
+      nextNode = mainNode.children[0]
+    }
+
+  return mainNode
+}
